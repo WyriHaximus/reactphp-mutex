584641760736c065128458252e064f0f1eb8e694
Cees-Jan Kiewiet <ceesjank@gmail.com>
2021-01-06 21:27:28 +0100

    Change test trait into abstract test case

--- tests/MemoryTest.php
+++ tests/MemoryTest.php
@@ -5,15 +5,12 @@ declare(strict_types=1);
 namespace WyriHaximus\Tests\React\Mutex;
 
 use React\EventLoop\LoopInterface;
-use WyriHaximus\AsyncTestUtilities\AsyncTestCase;
+use WyriHaximus\React\Mutex\AbstractMutexTestCase;
 use WyriHaximus\React\Mutex\Memory;
 use WyriHaximus\React\Mutex\MutexInterface;
-use WyriHaximus\React\Mutex\MutexTestsTrait;
 
-final class MemoryTest extends AsyncTestCase
+final class MemoryTest extends AbstractMutexTestCase
 {
-    use MutexTestsTrait;
-
     public function provideMutex(LoopInterface $loop): MutexInterface
     {
         return new Memory();
